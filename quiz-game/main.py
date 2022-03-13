64c84bb4fc9c1536fba638808139c816b94f2b2a
tolgaasahinn <97322196+tolgaasahinn@users.noreply.github.com>
2022-03-13 14:01:45 +0300

    quiz game true false

---
+++ quiz-game/main.py
@@ -0,0 +1,25 @@
+from data import question_data
+from question_model import Question
+from quiz_brain import QuizBrain
+from art import logo
+
+print(logo)
+print("""
+Please Choose Your Topic
+1. General Knowledge
+2. Celebrities
+3. History
+4. Science
+
+""")
+topic = input("Please Choose Your Topic:")
+question_bank = []
+topic = int(topic)
+for i in range(len(question_data[topic])):
+    question = Question(question_data[topic][i]["question"], question_data[topic][i]["correct_answer"])
+    question_bank.append(question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_quesiton():
+    quiz.next_question()
